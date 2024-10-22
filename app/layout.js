e9e46a1f4212abb6ed8ac193a8518aa35fb78128
Praveen Tiwari <praveentiwari29004@gmail.com>
2024-10-22 14:03:51 +0530

    created a user form inside event section

--- app/layout.js
+++ app/layout.js
@@ -2,6 +2,7 @@ import {Inter } from "next/font/google"
 import "./globals.css";
 import Header from "@/components/header";
 import { ClerkProvider } from "@clerk/nextjs";
+import CreateEventDrawer from "@/components/create-event";
 
 
 const inter = Inter({subsets: ["latin"]});
@@ -25,6 +26,7 @@ export default function RootLayout({ children }) {
             </p>
           </div>
         </footer>
+        <CreateEventDrawer />
       </body>
     </html>
     </ClerkProvider>
