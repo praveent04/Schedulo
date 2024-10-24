735a99c9061b5600b2925c871c61cb7fa15bc849
Praveen Tiwari <praveentiwari29004@gmail.com>
2024-10-25 01:51:37 +0530

    done with calendar connect and meet logic meet link creation too

--- app/(main)/availability/layout.jsx
+++ app/(main)/availability/layout.jsx
@@ -3,7 +3,7 @@ import { Suspense } from "react";
 export default async function AvailabilityLayout({ children }) {
   return (
     <div className="mx-auto">
-      <Suspense fallback={<div>Loading availability...</div>}>
+      <Suspense fallback={<div>Loading Availability...</div>}>
         {children}
       </Suspense>
     </div>
